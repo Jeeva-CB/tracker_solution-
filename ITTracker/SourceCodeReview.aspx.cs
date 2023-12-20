b41e29d76fc61183346f768fd222270800fa7f70
375468 <375468@manappuram.com>
2023-12-20 09:48:25 +0530

    commit j

---
+++ ITTracker/SourceCodeReview.aspx.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using System.Web.Services;
+
+namespace ITTracker
+{   
+    public partial class SourceCodeReview : System.Web.UI.Page
+    {
+        public static string editContent;
+        public void Page_Init(object o, EventArgs e)
+        {
+
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+
+            Response.Cache.SetExpires(DateTime.UtcNow.AddHours(-1));
+
+            Response.Cache.SetNoStore();
+        }
+
+        // load on method
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string usr, bid, fimid, ipaddres, sessn;
+            if (string.IsNullOrEmpty(Session["username"] as string))
+            {
+                Response.Redirect("SessionExpired.aspx");
+            }
+            else
+            {
+                usr = Session["username"].ToString();
+                bid = Session["branch_id"].ToString();
+                fimid = Session["firm_id"].ToString();
+                ipaddres = Session["system_ip"].ToString();
+                sessn = Session["sessionkey"].ToString();
+                this.hdUserId.Value = usr;
+                this.hdBranchId.Value = bid;
+                this.hdFirmId.Value = fimid;
+                this.hdnSystemIP.Value = ipaddres;
+                this.hdSesssion.Value = sessn;
+
+            }
+        }
+        public class getDropDownData
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+        }
+        [WebMethod(EnableSession = true)]
+        public static List<getDropDownData> getFillData(string pageVal, string pageval1)
+        {
+            DataSet ds;
+            List<getDropDownData> getData = new List<getDropDownData>();
+            ITService.ITServiceClient obj1 = new ITService.ITServiceClient();
+            ds = obj1.TrackerSelect(pageVal, pageval1, "", "", "");
+            //PWA_Service.PWA_ServiceClient obj1 = new PWA_Service.PWA_ServiceClient();
+            //ds = obj1.PwaSelectData("PWAAPP", pageVal, pageval1, "", "");
+            try
+            {
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    foreach (DataRow dr in ds.Tables[0].Rows)
+                    {
+                        getData.Add(new getDropDownData()
+                        {
+                            id = dr[0].ToString(),
+                            name = dr[1].ToString()
+                        });
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            return getData;
+        }
+
+
+        [WebMethod(EnableSession = true)]
+        public static string getRequestNote(string pageVal, string pageval1, string pageval2)
+        {
+            DataSet ds;
+            string str = "";
+            ITService.ITServiceClient obj1 = new ITService.ITServiceClient();
+            ds = obj1.TrackerSelect(pageVal, pageval1, pageval2, "", "");
+
+            try
+            {
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    str = ds.Tables[0].Rows[0][0].ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+            return str;
+        }
+
+
+
+        [WebMethod(EnableSession = true)]
+        public static string UploadingFiles(string ImageData, string InputData)
+        {
+            String InputDataDecrypted = ClientsideEncryption.AESEncrytDecry.DecryptStringAES(InputData);
+            //List<string> extentions = new List<string>();
+            //extentions.Add("913020010321699");
+
+            List<string> extentions = new List<string>();
+
+            extentions.InsertRange(extentions.Count, new string[] { "pdf", "jpg", "gif", "jpeg", "bmp", "tif", "tiff", "png", "xps", "doc", "docx", "fax", "wmp", "ico", "txt", "rtf", "xls", "xlsx", "ppt", "pptx", "odc", "odt" });
+
+            string InputDataExt = InputDataDecrypted.Split('µ')[2];
+            try
+            {
+                if (extentions.Contains(InputDataExt))
+                {
+                    string result = "";
+                    string InputString = ImageData.Split(',')[1];
+                    Byte[] imgByte = Convert.FromBase64String(InputString);
+                    ITService.ITServiceClient obj1 = new ITService.ITServiceClient();
+
+                    result = obj1.TrackerDocumentUpload("51", InputDataDecrypted, imgByte);
+                    return result;
+                }
+                else
+                {
+                    return "666";
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+        }
+
+
+        //confirm button
+        [WebMethod(EnableSession = true)]
+        public static string confirm(string pageVal, string pageval1, string pageval2, string pageval3)
+        {
+            DataSet ds, ds1, ds2, ds3;
+            string str = "", newcontent = "";
+            ITService.ITServiceClient obj1 = new ITService.ITServiceClient();
+            ds = obj1.TrackerSelect(pageVal, pageVal, pageval1, pageval2, pageval3);
+            //string[] notes = pageval1.Split('^');
+            //string noteid = notes[2].ToString();
+            //string status = "";
+            //string dev = notes[3].ToString();
+            try
+            {
+                  if (ds.Tables[0].Rows.Count > 0)
+                    {
+                        str = ds.Tables[0].Rows[0][0].ToString();
+                    
+                    }
+                    
+                
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+            return str;
+        }
+
+
+
+    }
+}
